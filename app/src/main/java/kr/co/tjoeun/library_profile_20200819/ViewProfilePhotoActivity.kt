ce23a4a0721897d69031b12bb810eb13071600df
EUNYOUNGYOON <milkoon1@naver.com>
2020-08-19 20:01:38 +0900

    사진을 클릭하면 큰 이미지 있는 사진으로 이동

---
+++ app/src/main/java/kr/co/tjoeun/library_profile_20200819/ViewProfilePhotoActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.library_profile_20200819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfilePhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_photo)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
